c1808830ea9b3c877abd57326e90bd02a121c51b
zjakubb <jakub.zuchowicz@student.uj.edu.pl>
2023-12-22 21:09:35 +0100

    initial commit

---
+++ src/main/java/com/jakubzuchowicz/FCBService/FcbServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jakubzuchowicz.FCBService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcbServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FcbServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jakubzuchowicz/FCBService/FcbServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakubzuchowicz.FCBService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcbServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
