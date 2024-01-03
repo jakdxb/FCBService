41d66fe5bc5f3dd33d48acb4417b8d6e3f1bcc43
zjakubb <jakub.zuchowicz@student.uj.edu.pl>
2024-01-04 00:29:38 +0100

    added Article and Comment model classes

--- src/main/java/com/jakubzuchowicz/FCBService/model/Article.java
+++ src/main/java/com/jakubzuchowicz/FCBService/model/Article.java
@@ -1,4 +1,26 @@
 package com.jakubzuchowicz.FCBService.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
 public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "article_id")
+    private List<Comment> comments = new ArrayList<>();
 }
--- src/main/java/com/jakubzuchowicz/FCBService/model/Comment.java
+++ src/main/java/com/jakubzuchowicz/FCBService/model/Comment.java
@@ -1,4 +1,15 @@
 package com.jakubzuchowicz.FCBService.model;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@Entity
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String text;
 }
